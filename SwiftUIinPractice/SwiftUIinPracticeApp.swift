8b677dadf1d3ae3a3f65bd51d08a0754a3e5c10f
Berkay Tuncel <berkaytncl98@gmail.com>
2024-05-13 16:46:58 +0300

    Initial Commit

---
+++ SwiftUIinPractice/SwiftUIinPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIinPracticeApp.swift
+//  SwiftUIinPractice
+//
+//  Created by Berkay Tuncel on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIinPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
